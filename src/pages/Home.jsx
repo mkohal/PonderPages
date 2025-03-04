238f363923cfdfbd69e110ab7b2d31130942ae35
Maheshwar Kohal <maheshwarkohal8@gmail.com>
2025-03-04 23:34:07 +0530

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,10 @@
 import React from "react";
+import img1 from "../assets/1.png";
+import img2 from "../assets/2.png";
+import img3 from "../assets/3.png";
+import img4 from "../assets/4.png";
 
-const images = [
-  "/src/assets/1.png",
-  "/src/assets/2.png",
-  "/src/assets/3.png",
-  "/src/assets/4.png",
-
-];
+const images = [img1, img2, img3, img4];
 
 const Home = () => {
   return (
@@ -29,4 +27,3 @@ const Home = () => {
 };
 
 export default Home;
-
